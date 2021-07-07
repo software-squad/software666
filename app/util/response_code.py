e2fd1f07083aae5b03627b81b9fb77e3ee2bd37d
Maxpicca-Li <liyanqin@cqu.edu.cn>
2021-07-07 09:46:02 +0800

    响应体修改2

--- app/util/response_code.py
+++ app/util/response_code.py
@@ -2,18 +2,23 @@ from fastapi import status
 from fastapi.responses import JSONResponse, Response  # , ORJSONResponse
 from typing import Union
 
-# 注意有个 * 号 不是笔误， 意思是调用的时候要指定参数 e.g.resp_200（data=xxxx)
-def res_200(data:object = None,msg:str="Succeed") -> Response:
-    return result(200,msg,data)
-    
-def res_400(data:object = None, msg: str="Error") -> Response:
-    return result(400,msg,data)
+from starlette.status import HTTP_200_OK
 
-def result(code:int, msg: str = None, data:object = None) -> Response:
+def res_200(data:object = None,msg:str="Succeed") -> Response:
     return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
+        status_code=status.HTTP_200_OK,
         content={
-            'code': code,
+            'code': 200,
+            'message': msg,
+            'data': data,
+        }
+    )
+    
+def res_400(data:object = None, msg: str="BAD REQUEST") -> Response:
+    return JSONResponse(
+        status_code=status.HTTP_400_OK,
+        content={
+            'code': 400,
             'message': msg,
             'data': data,
         }
