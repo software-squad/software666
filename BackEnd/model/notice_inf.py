3b4960de3c1fa6f644ed39c86b9c61629c40a4fe
Y-Staring <69038431+Y-Staring@users.noreply.github.com>
2021-07-11 20:00:12 +0800

    Update notice_inf.py

--- BackEnd/model/notice_inf.py
+++ BackEnd/model/notice_inf.py
@@ -2,9 +2,19 @@ from pydantic import BaseModel
 
 
 class NoticeInf(BaseModel):
-    id: int
+    noticeid: int
     title: str
     content: str
     createdate: str
-    userid: str
+    userid: int
     username: str
+
+class DelNoticeInf(BaseModel):
+    noticeid:int
+
+class AddNoticeInf(BaseModel):
+    title:str
+    content:str
+    createdate:str
+    userid:int
+    username:str
