6a26efed772a55bddae6fb7275cfd849a69046be
Y-Staring <69038431+Y-Staring@users.noreply.github.com>
2021-07-11 20:01:59 +0800

    Update noticeService.py

--- BackEnd/service/noticeService.py
+++ BackEnd/service/noticeService.py
@@ -15,21 +15,21 @@ def showNotices():
 #     return result
 
 
-def editNotice(noticeid, title, createdate, content, userid, username):
+def editNotice(notice):
     print("====编辑单个公告信息====")
-    res = noticeDao.editOneNotice(noticeid, title, createdate,
-                                  content, userid, username)
+    res = noticeDao.editOneNotice(notice.noticeid, notice.title, notice.createdate,
+                                  notice.content, notice.userid, notice.username)
     # 返回是否能成功编辑
     return res
 
 
-def delNotice(noticeid):
+def delNotice(notice):
     print("====删除单个公告信息====")
-    res = noticeDao.delOneNotice(noticeid)
+    res = noticeDao.delOneNotice(notice.noticeid)
     return res
 
 
-def addNotice(title, content, createdate, userid, username):
+def addNotice(notice):
     print("====增加单个公告信息====")
-    res = noticeDao.addOneNotice(title, content, createdate, userid, username)
+    res = noticeDao.addOneNotice(notice.title, notice.content, notice.createdate, notice.userid, notice.username)
     return res
