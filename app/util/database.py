a321fde495dfb6c6a95a743102ffc6c0e84bb7f1
Maxpicca-Li <liyanqin@cqu.edu.cn>
2021-07-11 23:10:03 +0800

    隔离前端文件夹dev-view

--- app/util/database.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import config
-def getUserDB():
-    # 打开数据库连接
-    conn = pymysql.connect(host=config.url, user="root", passwd="123456", database="test")
-
-    # 使用cursor()方法获取操作游标
-    db = conn.cursor()
-    return db
