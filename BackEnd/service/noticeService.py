10b1be9f5caf6062b465a98cf3239f7154784fd7
Y-Staring <69038431+Y-Staring@users.noreply.github.com>
2021-07-11 09:06:58 +0800

    Update noticeService.py

--- BackEnd/service/noticeService.py
+++ BackEnd/service/noticeService.py
@@ -1,26 +1,33 @@
-from dao import noticeDao
+import sys
+sys.path.append('D:\\python and c++\\software666-master\\CSI\\dao')   #括号内容为文件的绝对路径
+import noticeDao
+
+#XXX 还要增加管理员权限的判断
+def showNotices():
+    print("====展示多个公告信息====")
+    info,res=noticeDao.selAllNotices()
+    return info,res
 
 
-def showNotices(notice):
-    result = True
-    return result
+# def showOneNotice(notice):
+#     result = True
+#     return result
 
 
-def showOneNotice(notice):
-    result = True
-    return result
+def editNotice(noticeid,title,createdate,content,userid):
+    print("====编辑单个公告信息====")
+    res=noticeDao.editOneNotice(noticeid,title,createdate,content,userid)
+    #返回是否能成功编辑
+    return res
 
 
-def editNotice(notice):
-    result = True
-    return result
+def delNotice(noticeid):
+    print("====删除单个公告信息====")
+    res=noticeDao.delOneNotice(noticeid)
+    return res
 
 
-def delNotice(notice):
-    result = True
-    return result
-
-
-def addNotice(notice):
-    result = True
-    return result
+def addNotice(title,content,createdate,userid):
+    print("====增加单个公告信息====")
+    res=noticeDao.addOneNotice(title,content,createdate,userid)
+    return res
