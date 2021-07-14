d427d74e63336156d46f5e821e0810cf9a305959
StrivingandStudying <1506760246@qq.com>
2021-07-14 09:22:30 +0800

    最新版后端，dao层修改

--- BackEnd/util/database.py
+++ BackEnd/util/database.py
@@ -1,7 +1,15 @@
 import pymysql
 
 
-def connectToDataBase(host="192.168.0.102", user="root",
+# def connectToDataBase(host="4097n16y34.zicp.vip", user="root",
+#                       password="123456", database="software666", port=80):
+#     db = pymysql.connect(host=host, user=user,
+#                          passwd=password, database=database, port=port)
+#     cursor = db.cursor(pymysql.cursors.DictCursor)
+#     return db, cursor
+
+
+def connectToDataBase(host="192.168.0.113", user="root",
                       password="123456", database="software666"):
     db = pymysql.connect(host=host, user=user,
                          passwd=password, database=database)
